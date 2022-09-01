13e971a357262934ad2803122a6244055be4ed25
r-avellar <renatoavellar5@gmail.com>
2022-08-31 23:06:28 -0300

    refatorando

--- exe04.java
+++ exe04.java
@@ -4,13 +4,13 @@ public class exe04 {
 
     public static void main(String[] args) {
         Random random = new Random();
-        Stack pilha1 = new Stack();
-        Stack pilha2 = new Stack();
+        Stack<Integer> pilha1 = new Stack<>();
+        Stack<Integer> pilha2 = new Stack<>();
         Scanner ler = new Scanner(System.in);
 
 
         for (int i = 0; i < 10; i++) {
-            int poem = (int) pilha1.push(random.nextInt(100));
+            pilha1.push(random.nextInt(100));
         }
 
 
@@ -22,16 +22,19 @@ public class exe04 {
         int x = (pilha1.search(n));
 
         for (int i = 0; i < x; i++) {
-            int tira = (int) pilha1.pop();
-            int poem = (int) pilha2.push(tira);
-        }
+//            int tira = pilha1.pop();
+//            pilha2.push(tira);
 
-        int a = (int) pilha2.pop();
-        System.out.println(a);
+            pilha2.push(pilha1.pop());
+        }
+        System.out.println("retirando numero....");
+        pilha2.pop();
 
         for (int i = 0; i < x-1; i++) {
-            int tira = (int) pilha2.pop();
-            int poem = (int) pilha1.push(tira);
+//            int tira = pilha2.pop();
+//            pilha1.push(tira);
+
+            pilha1.push(pilha2.pop());
         }
 
         System.out.println("pilha : " + pilha1);
